149412ef1afc0137a393fb3144f2a128c126c4ce
TheZoq2 <frans.skarman@gmail.com>
2018-04-19 23:41:03 +0200

    Disable notifications

--- sendPhoto.sh
+++ sendPhoto.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 url="https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/sendPhoto"
-curl -X POST "$url" -F chat_id="$1" -F photo="@$2"
+curl -X POST "$url" -F chat_id="$1" -F photo="@$2" -F disable_notification=true
